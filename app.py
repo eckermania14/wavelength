9c8676886bbbd510a100f329eacacc50c42ceb11
eckermania14 <eckermania514@gmail.com>
2026-07-18 17:36:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -322,4 +322,4 @@ init_db()
 
 # Run
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=False, allow_unsafe_werkzeug=True)
